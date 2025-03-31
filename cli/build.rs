2dfb71b5067b0f97ab552de67dddddc6c8d81916
Vijay Ananth <vijay.hassan@gmail.com>
2025-04-01 00:37:40 +0530

    Provide a client library and an example app using the client lib.

--- cli/build.rs
+++ cli/build.rs
@@ -1,9 +1,9 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(false)
-        //.out_dir("./src")
         .compile_protos(
-            &["proto/confer.proto"],
-            &["proto"])?;
+            &["../proto/confer.proto"],
+            &["../proto"],
+        )?;
     Ok(())
 }
